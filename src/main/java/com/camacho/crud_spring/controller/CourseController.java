22af5df4a74308ffbd0db5efe4bcbb8780510229
Marcelo Santana Camacho <marcelo.camacho@serpro.gov.br>
2024-12-01 11:57:26 -0300

    retorna dados de um curso específico pelo id /courses/ID

--- src/main/java/com/camacho/crud_spring/controller/CourseController.java
+++ src/main/java/com/camacho/crud_spring/controller/CourseController.java
@@ -9,9 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 import com.camacho.crud_spring.model.Course;
 import com.camacho.crud_spring.repository.CourseRepository;
 
+import jakarta.websocket.server.PathParam;
 import lombok.AllArgsConstructor;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -24,5 +31,25 @@ public class CourseController {
     @GetMapping
     public @ResponseBody List<Course> list(){
         return courseRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> getCourse(@PathVariable Long id){
+        return courseRepository.findById(id)
+            .map(record -> ResponseEntity.ok().body(record))
+            .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity create(@RequestBody Course course){
+        List<Course> allCourse = courseRepository.findAll();
+        Course existCourse = allCourse.stream().filter(c -> c.getName().equals(course.getName())).findFirst().orElse(null);
+        if(existCourse!=null){
+            throw new RuntimeException("Já existe um curso com o título informado");
+        }
+        //return courseRepository.save(course);
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+            courseRepository.save(course)
+            );
     }
 }
